f21ea25133592bf124ff979f73086d5e65134480
Валаев Георгий <ymnikgoga@gmail.com>
2022-12-17 22:30:50 +0300

    Добавлены методы для работы с селениумом

--- src/main/java/helpers/SeleniumHelper.java
+++ src/main/java/helpers/SeleniumHelper.java
@@ -1,17 +1,20 @@
 package helpers;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static drivermanager.DriverManager.getDriver;
-import static drivermanager.DriverManager.getDriverWait;
 
 public class SeleniumHelper {
 
-    public static void openPage(String page, By locator) {
+    public static void openPage(String page) {
         getDriver().get(page);
-        getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
+    public static void sendKeysToInputField(String stringToSend, By locator) {
+        getDriver().findElement(locator).sendKeys(stringToSend);
+    }
 
+    public static void clickButton(By locator) {
+        getDriver().findElement(locator).click();
+    }
 }
