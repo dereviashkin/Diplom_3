edd49883c3be43ec0dc405f721cc742ad0b6cd75
Валаев Георгий <ymnikgoga@gmail.com>
2022-12-17 23:27:43 +0300

    Разработан класс LoginPage.java

--- src/main/java/pageobject/LoginPage.java
+++ src/main/java/pageobject/LoginPage.java
@@ -4,16 +4,35 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 import static helpers.ChecksHelper.waitForElementToBeVisible;
+import static helpers.SeleniumHelper.clickButton;
+import static helpers.SeleniumHelper.sendKeysToInputField;
+import static helpers.UserHelper.getCurrentEmail;
+import static helpers.UserHelper.getCurrentPassword;
 
 public class LoginPage {
 
-    static final String loginUrl = "https://stellarburgers.nomoreparties.site/login";
+    static final String loginPageUrl = "https://stellarburgers.nomoreparties.site/login";
 
     static final By loginButton = By.xpath(".//button[text() = 'Войти']");
+    static final By inputFieldEmail = By.xpath(".//label[text() = 'Email']/following-sibling::input");
+    static final By inputFieldPassword = By.xpath(".//label[text() = 'Пароль']/following-sibling::input");
 
     @Step("Ждем, что видна кнопка \"Войти\"")
     public LoginPage waitForRegistrationIsSuccess() {
         waitForElementToBeVisible(loginButton);
+        return this;
+    }
+
+    @Step("Заполняем поля данными созданной учетки")
+    public LoginPage loginWithCreatedAccount() {
+        sendKeysToInputField(getCurrentEmail(), inputFieldEmail);
+        sendKeysToInputField(getCurrentPassword(), inputFieldPassword);
+        return this;
+    }
+
+    @Step("Кликаем по кнопке \"Войти\"")
+    public LoginPage clickLoginButton() {
+        clickButton(loginButton);
         return this;
     }
 }
