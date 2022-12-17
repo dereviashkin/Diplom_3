92db6447b75c5d5855aa8dfd60df64a6395cae0f
Валаев Георгий <ymnikgoga@gmail.com>
2022-12-17 22:30:33 +0300

    Разработан класс RegisterPage.java, RegisterPageTest.java

--- src/main/java/pageobject/RegisterPage.java
+++ src/main/java/pageobject/RegisterPage.java
@@ -3,18 +3,58 @@ package pageobject;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
-import static helpers.SeleniumHelper.openPage;
+import static helpers.ChecksHelper.waitForElementToBeVisible;
+import static helpers.GeneratorHelper.*;
+import static helpers.SeleniumHelper.*;
 
 
 public class RegisterPage {
 
-    public static final String registerUrl = "https://stellarburgers.nomoreparties.site/register";
+    static final String registerUrl = "https://stellarburgers.nomoreparties.site/register";
 
-    public static final By registerButton = By.xpath(".//button[text() = 'Зарегистрироваться']");
+    static final By buttonRegister = By.xpath(".//button[text() = 'Зарегистрироваться']");
+    static final By inputFieldName = By.xpath(".//label[text() = 'Имя']/following-sibling::input");
+    static final By inputFieldEmail = By.xpath(".//label[text() = 'Email']/following-sibling::input");
+    static final By inputFieldPassword = By.xpath(".//label[text() = 'Пароль']/following-sibling::input");
+    static final By messageIncorrectPassword = By.xpath(".//p[text() = 'Некорректный пароль']");
 
-    @Step("Открываем страницу регистрации и ждем отображения кнопки \"Зарегистрироваться\"")
+    @Step("Открываем страницу регистрации и ")
     public RegisterPage openRegisterPage() {
-        openPage(registerUrl, registerButton);
+        openPage(registerUrl);
+        return this;
+    }
+
+    @Step("Ждем отображения кнопки \"Зарегистрироваться\"")
+    public RegisterPage waitForRegisterButtonToAppear() {
+        waitForElementToBeVisible(buttonRegister);
+        return this;
+    }
+
+    @Step("Заполняем поля для регистрации нового пользователя")
+    public RegisterPage fillRegisterForm() {
+        sendKeysToInputField(randomName(), inputFieldName);
+        sendKeysToInputField(randomEmail(), inputFieldEmail);
+        sendKeysToInputField(randomPassword(), inputFieldPassword);
+        return this;
+    }
+
+    @Step("Заполняем поля для регистрации нового пользователя, но пароль короткий")
+    public RegisterPage fillRegisterFormShortPass() {
+        sendKeysToInputField(randomName(), inputFieldName);
+        sendKeysToInputField(randomEmail(), inputFieldEmail);
+        sendKeysToInputField(randomPasswordShort(), inputFieldPassword);
+        return this;
+    }
+
+    @Step("Кликаем по кнопке \"Зарегистрироваться\"")
+    public RegisterPage clickRegisterButton() {
+        clickButton(buttonRegister);
+        return this;
+    }
+
+    @Step("Ждем отображения ошибки о некорректном пароле")
+    public RegisterPage waitForIncorrectPassMessageAppear() {
+        waitForElementToBeVisible(messageIncorrectPassword);
         return this;
     }
 }
--- src/test/java/RegisterPageTest.java
+++ src/test/java/RegisterPageTest.java
@@ -1,13 +1,30 @@
 import io.qameta.allure.Description;
 import org.junit.Test;
+import pageobject.LoginPage;
 import pageobject.RegisterPage;
 
 public class RegisterPageTest extends BaseTest {
+
     RegisterPage registerPage = new RegisterPage();
+    LoginPage loginPage = new LoginPage();
 
     @Test
-    @Description("Положительный тест открытия страницы регистрации")
-    public void openRegisterPage() {
-        registerPage.openRegisterPage();
+    @Description("Положительный тест регистрации нового пользователя")
+    public void registerNewUserCorrectSuccess() {
+        registerPage
+                .openRegisterPage()
+                .fillRegisterForm()
+                .clickRegisterButton();
+        loginPage.waitForRegistrationIsSuccess();
+    }
+
+    @Test
+    @Description("Негативный тест регистрации нового пользователя, пароль слишком короткий")
+    public void registerNewUserShortPassFail() {
+        registerPage
+                .openRegisterPage()
+                .fillRegisterFormShortPass()
+                .clickRegisterButton()
+                .waitForIncorrectPassMessageAppear();
     }
 }
