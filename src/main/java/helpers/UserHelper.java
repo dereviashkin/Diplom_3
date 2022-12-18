d8dc06614a9cda190e9a0e3a66cc10af2f1bce01
Валаев Георгий <ymnikgoga@gmail.com>
2022-12-18 17:37:07 +0300

    Добавлен вывод имени, почты и пароля

--- src/main/java/helpers/UserHelper.java
+++ src/main/java/helpers/UserHelper.java
@@ -10,6 +10,7 @@ public class UserHelper {
 
     public User getRandomUser() {
         user = new User(randomName(), randomEmail(), randomPassword());
+        System.out.printf("\nИмя: %s, Email: %s, Пароль: %s", user.getName(), user.getEmail(), user.getPassword());
         return user;
     }
 
